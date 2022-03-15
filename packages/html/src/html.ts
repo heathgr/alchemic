373cdaa86977174e0acfffea6d582879c93d721d
Grant Heath <heathgr@gmail.com>
2022-03-15 14:56:17 -0600

    Refactor and simplify HTML parser.

--- packages/html/src/html.ts
+++ packages/html/src/html.ts
@@ -1,96 +1,101 @@
 import { sanitizeHtmlExpression } from '@alchemic/utilities'
-import { HTMLParser, HTMLParserContext, HTMLParserMathGroups, TemplateExpression } from './html.types'
+import { HTMLParser, HTMLParserMathGroups, TemplateExpression } from './html.types'
 
-// TODO refactor to put all functions in a context where the context does not need to be passed from function to function?
-const parseAttributes = (attributes: string, element: HTMLElement) => {
-  // TODO make attributes name matcher adhere to HTML5 standard
-  const attributeNameMatch = attributes.match(/(?<fullMatch>^ *(?<name>[a-zA-z0-9]+) *)/)
-  const attributeValueMatch = attributes.match(/(?<fullMatch> *= *(?<opening>['"])(?<value>[^=]*)\k<opening>)/)
-  const name = attributeNameMatch?.groups?.name
-  const value = attributeValueMatch?.groups?.value
+const html: HTMLParser = (templateStrings, ...templateExpressions) => {
+  const elementStack: HTMLElement[] = []
+  let activeExpression: TemplateExpression = undefined
+  let activeString = ''
+  // TODO move active element pointer up here?
 
-  if (name) element.setAttribute(name, value || name)
-  else return
-
-  const theRest = attributes.substring(
-    (attributeNameMatch?.groups?.fullMatch?.length || 0) + (attributeValueMatch?.groups?.fullMatch?.length || 0),
-  )
+  const parseAttributes = (attributes: string, element: HTMLElement) => {
+    // TODO make attributes name matcher adhere to HTML5 standard
+    const attributeNameMatch = attributes.match(/(?<fullMatch>^ *(?<name>[a-zA-z0-9]+) *)/)
+    const attributeValueMatch = attributes.match(/(?<fullMatch> *= *(?<opening>['"])(?<value>[^=]*)\k<opening>)/)
+    const name = attributeNameMatch?.groups?.name
+    const value = attributeValueMatch?.groups?.value
   
-  parseAttributes(theRest, element)
-}
-
-const parseExpression = ({ elementStack, templateString }: HTMLParserContext, expression: TemplateExpression): HTMLParserContext => {
-  switch (typeof expression) {
-    case 'string': return { elementStack, templateString: templateString + sanitizeHtmlExpression(expression) }
-    case 'number': return { elementStack, templateString: templateString + expression }
-    case 'object': {
-      if (expression instanceof HTMLElement) {
-        elementStack[elementStack.length - 1].appendChild(expression)
-      }
-
-      return { elementStack, templateString }
-    }
-    default: return { elementStack, templateString }
-  }
-}
-
-const parseElements = ({ elementStack, templateString }: HTMLParserContext): HTMLParserContext => {
-  const match = templateString.match(/(?<fullMatch><(?<closing>\/?) *(?<tagName>\w*) *(?<attributes>[()a-zA-Z0-9 ="'-]*) *(?<isSelfClosing>\/?)>)/)
-
-  if (match) {
-    const { fullMatch, tagName, closing, attributes, isSelfClosing } = match.groups as HTMLParserMathGroups
-    const matchIndex = match.index || 0
-    const preMatchString = templateString.substring(0, matchIndex).trim()
-    const postMatchString = templateString.substring(matchIndex + fullMatch.length)
-    let activeElement = elementStack[elementStack.length - 1] // TODO use a pointer to handle active element?
-
-    if (preMatchString) activeElement.appendChild(document.createTextNode(preMatchString))
+    if (name) element.setAttribute(name, value || name)
+    else return
+  
+    const theRest = attributes.substring(
+      (attributeNameMatch?.groups?.fullMatch?.length || 0) + (attributeValueMatch?.groups?.fullMatch?.length || 0),
+    )
     
-    if (closing) {
-      if (tagName?.toLowerCase() !== activeElement.tagName.toLowerCase())
-        throw new Error(`Invalid HTML: opening tag ${activeElement.tagName.toLocaleLowerCase()} does not match closing tag ${tagName?.toLocaleLowerCase()}`)
-      if (elementStack.length > 1) {
-        elementStack[elementStack.length - 2].appendChild(activeElement)
-        elementStack.pop()
-        activeElement = elementStack[elementStack.length - 1]
-      }
-    } else {
-      elementStack.push(document.createElement(tagName as keyof HTMLElementTagNameMap))
-      activeElement = elementStack[elementStack.length - 1]
+    parseAttributes(theRest, element)
+  }
 
-      if (attributes) parseAttributes(attributes, activeElement)
-      if (isSelfClosing) {
+  const parseExpression = () => {
+    switch (typeof activeExpression) {
+      case 'string': {
+        activeString = activeString + sanitizeHtmlExpression(activeExpression)
+        return
+      }
+      case 'number': {
+        activeString = activeString + activeExpression
+        return
+      }
+      case 'object': {
+        if (activeExpression instanceof HTMLElement) {
+          elementStack[elementStack.length - 1].appendChild(activeExpression)
+        }
+  
+        return
+      }
+      default: return
+    }
+  }
+
+  const parseElements = () => {
+    const tagMatch = activeString.match(/(?<fullMatch><(?<closing>\/?) *(?<tagName>\w*) *(?<attributes>[()a-zA-Z0-9 ="'-]*) *(?<isSelfClosing>\/?)>)/)
+    
+    if (tagMatch) {
+      const { fullMatch, tagName, closing, attributes, isSelfClosing } = tagMatch.groups as HTMLParserMathGroups
+      const matchIndex = tagMatch.index || 0
+      const preMatchString = activeString.substring(0, matchIndex).trim()
+      const postMatchString = activeString.substring(matchIndex + fullMatch.length)
+      let activeElement = elementStack[elementStack.length - 1] // TODO use a pointer to handle active element?
+  
+      if (preMatchString) activeElement.appendChild(document.createTextNode(preMatchString))
+      
+      if (closing) {
+        if (tagName?.toLowerCase() !== activeElement.tagName.toLowerCase())
+          throw new Error(`Invalid HTML: opening tag ${activeElement.tagName.toLocaleLowerCase()} does not match closing tag ${tagName?.toLocaleLowerCase()}`)
         if (elementStack.length > 1) {
           elementStack[elementStack.length - 2].appendChild(activeElement)
           elementStack.pop()
           activeElement = elementStack[elementStack.length - 1]
         }
+      } else {
+        elementStack.push(document.createElement(tagName as keyof HTMLElementTagNameMap))
+        activeElement = elementStack[elementStack.length - 1]
+  
+        if (attributes) parseAttributes(attributes, activeElement)
+        if (isSelfClosing) {
+          if (elementStack.length > 1) {
+            // TODO this is duplicate logic clean it up
+            elementStack[elementStack.length - 2].appendChild(activeElement)
+            elementStack.pop()
+            activeElement = elementStack[elementStack.length - 1]
+          }
+        }
       }
+      
+      activeString = postMatchString
+      parseElements()
     }
   
-    return parseElements({ elementStack, templateString: postMatchString })
-  }
-
-  return { elementStack, templateString }
-}
-
-const html: HTMLParser = (templateStrings, ...templateExpressions) => {
-  let parserContext: HTMLParserContext = {
-    elementStack: [],
-    templateString: '',
+    return
   }
 
   for (let i = 0; i < templateStrings.length; i++) {
-    parserContext = parseElements({
-      ...parserContext,
-      templateString: parserContext.templateString + templateStrings[i],
-    })
-
-    parserContext = parseExpression(parserContext, templateExpressions[i])
+    activeString += templateStrings[i]
+    activeExpression = templateExpressions[i]
+    parseElements()
+    parseExpression()
   }
 
   // TODO handle templates with no root element
-  return parserContext.elementStack[0] || document.createDocumentFragment()
+  return elementStack[0] || document.createDocumentFragment()
 }
 
 export default html
--- packages/html/src/html.types.ts
+++ packages/html/src/html.types.ts
@@ -7,8 +7,3 @@ export type HTMLParsingElement = HTMLElement | DocumentFragment
 export type HTMLParser = (templateStrings: TemplateStringsArray, ...templateExpressions: TemplateExpression[]) => HTMLParsingElement
 
 export type HTMLParserMathGroups = { fullMatch: string, tagName?: string, closing?: string, attributes?: string, isSelfClosing?: string }
-
-export interface HTMLParserContext {
-  elementStack: HTMLElement[],
-  templateString: string,
-}
